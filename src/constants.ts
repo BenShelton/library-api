42334c1afecf28db70a6381a9064474747be6c48
BenShelton <bensheltonjones@gmail.com>
2021-02-17 10:57:03 +0000

    add methods to extract rows from the database

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,5 @@ import path from 'path'
 export const CATALOG_URL = 'https://download-a.akamaihd.net/meps/jwl/current/catalogs/v3/catalog.db.gz'
 export const ROOT_DIR = path.join(__dirname, '..')
 export const DOWNLOAD_DIR = path.join(ROOT_DIR, 'downloads')
+export const CATALOG_FILE = 'catalog.db'
+export const CATALOG_PATH = path.join(DOWNLOAD_DIR, CATALOG_FILE)
--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,22 @@
-import { CATALOG_URL } from './constants'
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+
+import { CATALOG_FILE, CATALOG_PATH, CATALOG_URL } from './constants'
 import { downloadFile } from './download'
 
 export async function updateDatabase (): Promise<void> {
-  await downloadFile(CATALOG_URL, 'catalog.db')
+  await downloadFile(CATALOG_URL, CATALOG_FILE)
+}
+
+function openDatabase (): ReturnType<typeof open> {
+  return open({
+    filename: CATALOG_PATH,
+    mode: sqlite3.OPEN_READONLY,
+    driver: sqlite3.cached.Database
+  })
+}
+
+export async function getRows<T> (query: string, params?: string | string[] | Record<string, string>): Promise<T[]> {
+  const db = await openDatabase()
+  return db.all<T[]>(query, params)
 }

eab8b926d2d0457890ffeaad5821a56dc27dc1cc
BenShelton <bensheltonjones@gmail.com>
2021-03-30 12:38:02 +0100

    feat: add oclm publication class

--- packages/core/src/Publication.ts
+++ packages/core/src/Publication.ts
@@ -7,7 +7,7 @@ import { PUBLICATION_CLASSES } from './constants'
 import { ArticleRow, ImageRow, VideoRow } from '../types/database'
 import { ImageDTO, VideoDTO } from '../types/dto'
 
-interface PublicationCtor {
+export interface PublicationCtor {
   filename: string
   dir: string
 }
@@ -15,7 +15,7 @@ interface PublicationCtor {
 /**
  * Provides methods for interacting with a downloaded publication.
  */
-class BasePublication {
+abstract class BasePublication {
   filename: string
   path: string
   _mapper: PublicationMapper
@@ -73,6 +73,49 @@ export class PublicationWT extends BasePublication implements Publication {
       SELECT DocumentId, ContextTitle, Title
       FROM Document
       WHERE D.Class IS ${PUBLICATION_CLASSES.WATCHTOWER_ARTICLE}`
+    const db = await this.getDatabase()
+    const rows = await db.all<ArticleRow[]>(query)
+    return rows
+  }
+}
+
+export class PublicationOCLM extends BasePublication implements Publication {
+  async getImages (date: string) {
+    const offsetDate = date.replace(/-/g, '')
+    // TODO: Update this query
+    const query = `
+      SELECT D.ContextTitle, M.Caption, M.FilePath
+      FROM Multimedia M
+      JOIN DocumentMultimedia DM ON M.MultimediaId = DM.MultimediaId
+      INNER JOIN Document D ON DM.DocumentId = D.DocumentId
+      INNER JOIN InternalLink IL ON IL.MepsDocumentId = D.MepsDocumentId
+      INNER JOIN DocumentInternalLink AS DIL ON DIL.InternalLinkId = IL.InternalLinkId
+      INNER JOIN DatedText AS DT ON DT.EndParagraphOrdinal = DIL.EndParagraphOrdinal
+      WHERE DT.FirstDateOffset <= '${offsetDate}' AND DT.LastDateOffset >= '${offsetDate}'`
+    const db = await this.getDatabase()
+    const rows = await db.all<ImageRow[]>(query)
+    return this._mapper.MapImages(rows)
+  }
+
+  async getVideos (date: string) {
+    const offsetDate = date.replace(/-/g, '')
+    // TODO: Update this query
+    const query = `
+      SELECT M.KeySymbol, M.Track, M.IssueTagNumber
+      FROM Multimedia M
+      JOIN DocumentMultimedia DM ON M.MultimediaId = DM.MultimediaId
+      INNER JOIN DatedText AS DT ON DT.BeginParagraphOrdinal = DM.BeginParagraphOrdinal
+      WHERE DM.DocumentId = 1 AND DT.FirstDateOffset <= '${offsetDate}' AND DT.LastDateOffset >= '${offsetDate}'`
+    const db = await this.getDatabase()
+    const rows = await db.all<VideoRow[]>(query)
+    return this._mapper.MapVideos(rows)
+  }
+
+  async getArticles () {
+    const query = `
+      SELECT DocumentId, ContextTitle, Title
+      FROM Document
+      WHERE D.Class IS ${PUBLICATION_CLASSES.OCLM_WEEK}`
     const db = await this.getDatabase()
     const rows = await db.all<ArticleRow[]>(query)
     return rows

ef26b556191e433f39796be29c0e5b79d37376e6
BenShelton <bensheltonjones@gmail.com>
2021-04-13 14:00:24 +0100

    feat(media): add basic control panel and display design outline

--- packages/media/app/renderer/src/pages/Display.vue
+++ packages/media/app/renderer/src/pages/Display.vue
@@ -1,5 +1,7 @@
 <template>
-  <p>Display</p>
+  <div class="display-wrapper">
+    <p>Nothing Displaying</p>
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,3 +11,15 @@ export default defineComponent({
   name: 'Display'
 })
 </script>
+
+<style scoped>
+.display-wrapper {
+  height: 100%;
+  width: 100%;
+  background-color: #111111;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+}
+</style>

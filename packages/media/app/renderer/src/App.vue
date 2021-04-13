38b56e76dda89863f3db8bd8d0c7e29dadb8e448
BenShelton <bensheltonjones@gmail.com>
2021-04-13 14:00:24 +0100

    feat(media): add basic control panel and display design outline

--- packages/media/app/renderer/src/App.vue
+++ packages/media/app/renderer/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-view />
+  <div class="container">
+    <router-view />
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,3 +11,34 @@ export default defineComponent({
   name: 'App'
 })
 </script>
+
+<style>
+:root {
+  --primary: #5E83FF;
+  --secondary: #24E8BD;
+  --button-active: #00E0FF;
+  --button-disabled: #449FE8;
+  --background: #666666;
+}
+
+html,
+body,
+#app {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  width: 100%;
+  height: 100%;
+  background-color: var(--background);
+}
+</style>
--- packages/media/app/renderer/src/pages/ControlPanel.vue
+++ packages/media/app/renderer/src/pages/ControlPanel.vue
@@ -1,5 +1,11 @@
 <template>
-  <p>Control Panel</p>
+  <div class="navbar">
+    <span>Library Media</span>
+    <img
+      class="icon"
+      src="@/assets/icon.svg"
+    >
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,3 +15,21 @@ export default defineComponent({
   name: 'ControlPanel'
 })
 </script>
+
+<style scoped>
+.navbar {
+  width: 100vw;
+  height: 64px;
+  background-color: var(--primary);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 8px;
+}
+
+.icon {
+  flex: 0 0 50px;
+  width: 50px;
+  height: 50px;
+}
+</style>

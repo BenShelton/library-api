ff821d1390c7f9775f4a18863b696223f086c28f
BenShelton <bensheltonjones@gmail.com>
2021-04-14 14:13:23 +0100

    feat(media): add intro page if catalog is not yet downloaded

---
+++ packages/media/app/main/src/constants.ts
@@ -0,0 +1,7 @@
+import { app } from 'electron'
+import path from 'path'
+
+export const ROOT_DIR = app.getPath('userData')
+export const DOWNLOAD_DIR = path.join(ROOT_DIR, 'downloads')
+export const CATALOG_FILE = 'catalog.db'
+export const CATALOG_PATH = path.join(DOWNLOAD_DIR, CATALOG_FILE)
---
+++ packages/media/app/renderer/src/pages/ControlPanel/Intro.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="intro">
+    <h3>Welcome to Library Media</h3>
+    <p>In order to start you will need to download the Publication Catalog. This is a large file (about 500MB) so it may take some time to download.</p>
+    <button
+      :disabled="downloading"
+      @click="downloadCatalog()"
+    >
+      Download Catalog
+    </button>
+    <div
+      class="progress-wrapper"
+      :style="{ opacity: downloading ? 1 : 0 }"
+    >
+      <div class="progress-bar" />
+      <p v-text="progress + '%'" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'Intro',
+
+  setup () {
+    const downloading = ref(false)
+    const progress = ref(0)
+    async function downloadCatalog () {
+      downloading.value = true
+    }
+
+    return {
+      downloading,
+      progress,
+      downloadCatalog
+    }
+  }
+})
+</script>
+
+<style scoped>
+.intro {
+  flex: 1 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-flow: column nowrap;
+  text-align: center;
+  padding: 24px;
+}
+.progress-wrapper {
+  flex: 0 0 64px;
+}
+.progress-bar {
+  background-color: var(--primary);
+}
+</style>

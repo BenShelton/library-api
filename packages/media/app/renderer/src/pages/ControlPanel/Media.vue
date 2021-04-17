5cc6b560888b4b4915263c5f93052bc79fc21c16
BenShelton <bensheltonjones@gmail.com>
2021-04-17 17:56:01 +0100

    feat(media): initial install of catalog from intro page, simple media page

---
+++ packages/media/app/renderer/src/pages/ControlPanel/Media.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="media">
+    <p>Media Page</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Media'
+})
+</script>
+
+<style scoped>
+.media {
+  flex: 1 0 auto;
+  display: flex;
+  align-items: center;
+  flex-flow: column nowrap;
+  text-align: center;
+  padding: 24px;
+}
+</style>

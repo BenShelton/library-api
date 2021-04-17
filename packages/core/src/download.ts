2215732dbb35b15ac2b35c70c4a296e2205a3dad
BenShelton <bensheltonjones@gmail.com>
2021-04-17 17:18:10 +0100

    refactor: move download module from express to core

--- packages/core/src/download.ts
+++ packages/core/src/download.ts
@@ -6,11 +6,10 @@ import { promisify } from 'util'
 import { createGunzip } from 'zlib'
 import fetch, { Response } from 'node-fetch'
 import { Extract } from 'unzipper'
-import { CATALOG_URL, PUBLICATION_URL } from '@library-api/core'
-import { GetMediaPubLinks } from '@library-api/core/types/hag'
-import { VideoDTO } from '@library-api/core/types/dto'
 
-import { DOWNLOAD_DIR } from './constants'
+import { CATALOG_URL, PUBLICATION_URL } from './constants'
+import { GetMediaPubLinks } from '../types/hag'
+import { VideoDTO } from '../types/dto'
 
 const streamPipeline = promisify(pipeline)
 
@@ -22,13 +21,13 @@ function checkStatus (res: Response): Response {
   }
 }
 
-export async function downloadCatalog (): Promise<void> {
+export async function downloadCatalog (path: string): Promise<void> {
   const res = await fetch(CATALOG_URL)
   checkStatus(res)
   await streamPipeline(
     res.body,
     createGunzip(),
-    createWriteStream(join(DOWNLOAD_DIR, 'catalog.db'))
+    createWriteStream(path)
   )
 }
 

af4373584ab33d10a3995694d75f83d4fe58e115
BenShelton <bensheltonjones@gmail.com>
2021-05-03 12:30:12 +0100

    feat(media): scaffold video preview and displaying

--- packages/media/app/main/src/ipc.ts
+++ packages/media/app/main/src/ipc.ts
@@ -6,7 +6,7 @@ import { CatalogDatabase, updateCatalog } from '@library-api/core'
 import { getDisplayWindow } from './window'
 import { CATALOG_PATH, DOWNLOAD_DIR } from './constants'
 
-import { CatalogUpdate, DisplayImage, MediaImage, PublicationMedia } from '../../../types/ipc'
+import { CatalogUpdate, DisplayImage, DisplayVideo, MediaImage, MediaVideo, PublicationMedia } from '../../../types/ipc'
 
 export function initIPC (): void {
   ipcMain.handle('catalog:update', async (): Promise<CatalogUpdate['Response']> => {
@@ -23,14 +23,30 @@ export function initIPC (): void {
     const db = new CatalogDatabase(CATALOG_PATH)
     const publication = await db.getPublication(args.date, DOWNLOAD_DIR, args.type)
     if (!publication) return null
+
     const baseImages = await publication.getImages(args.date)
     const images = await Promise.all(baseImages.map(async (image) => {
       const srcURL = join(publication.contentsPath, image.filePath)
       const src = await readFile(srcURL, { encoding: 'base64' })
-      return { ...image, src: 'data:image/jpeg;base64,' + src }
+      return {
+        ...image,
+        src: 'data:image/jpeg;base64,' + src,
+        text: image.caption
+      }
     }))
+
+    const baseVideos = await publication.getVideos(args.date)
+    const videos = await Promise.all(baseVideos.map(async (video) => {
+      const src = ''
+      return {
+        ...video,
+        src: 'data:image/jpeg;base64,' + src,
+        text: 'Video'
+      }
+    }))
+
     return {
-      videos: await publication.getVideos(args.date),
+      videos,
       images
     }
   })
@@ -38,6 +54,11 @@ export function initIPC (): void {
   ipcMain.on('media:image', async (_event, args: MediaImage['Args']) => {
     const displayWindow = await getDisplayWindow()
     displayWindow.webContents.send('display:image', { src: args.src } as DisplayImage['Args'])
+  })
+
+  ipcMain.on('media:video', async (_event, args: MediaVideo['Args']) => {
+    const displayWindow = await getDisplayWindow()
+    displayWindow.webContents.send('display:video', { src: args.src } as DisplayVideo['Args'])
   })
 
   ipcMain.on('media:clear', async () => {

71e772a1b4804640e6866ca96e2e870007910cbc
BenShelton <bensheltonjones@gmail.com>
2021-05-04 22:28:29 +0100

    feat(media/display): make window frameless, lock aspect ratio, persist window positions in store

---
+++ packages/media/types/store.d.ts
@@ -0,0 +1,11 @@
+interface WindowPosition {
+  x: number
+  y: number
+  width: number
+  height: number
+}
+
+export interface StoreDefinition {
+  controlWindow: WindowPosition
+  displayWindow: WindowPosition
+}

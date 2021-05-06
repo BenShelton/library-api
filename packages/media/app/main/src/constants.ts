04cbdd54d29d1f4bb3bdb2d2b7d272a589838212
BenShelton <bensheltonjones@gmail.com>
2021-05-06 13:55:59 +0100

    feat(media): download video images & send captions

--- packages/media/app/main/src/constants.ts
+++ packages/media/app/main/src/constants.ts
@@ -3,5 +3,6 @@ import path from 'path'
 
 export const ROOT_DIR = app.getPath('userData')
 export const DOWNLOAD_DIR = path.join(ROOT_DIR, 'downloads')
+export const VIDEO_DIR = path.join(DOWNLOAD_DIR, 'videos')
 export const CATALOG_FILE = 'catalog.db'
 export const CATALOG_PATH = path.join(DOWNLOAD_DIR, CATALOG_FILE)

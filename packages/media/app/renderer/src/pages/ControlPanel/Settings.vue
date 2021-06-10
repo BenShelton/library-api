8269ebbee6fae4de5f93dad71e525c80aaadd3a1
BenShelton <bensheltonjones@gmail.com>
2021-06-10 12:00:09 +0100

    fix(media/settings): correct alignment of selects & buttons, add dividers between settings

--- packages/media/app/renderer/src/pages/ControlPanel/Settings.vue
+++ packages/media/app/renderer/src/pages/ControlPanel/Settings.vue
@@ -4,34 +4,38 @@
       SETTINGS
     </h1>
     <h2>Media</h2>
-    <div class="settings-line">
-      <p>
-        Hide images not normally shown during the meeting (cover images, publication covers etc.)
-      </p>
-      <select
-        v-model="settings.showImages"
-      >
-        <option value="all">
-          Show all
-        </option>
-        <option value="display">
-          Only show relevant
-        </option>
-      </select>
+    <div>
+      <div class="settings-line">
+        <p>
+          Hide images not normally shown during the meeting (cover images, publication covers etc.)
+        </p>
+        <select
+          v-model="settings.showImages"
+        >
+          <option value="all">
+            Show all
+          </option>
+          <option value="display">
+            Only show relevant
+          </option>
+        </select>
+      </div>
     </div>
     <h2>Reset</h2>
-    <div class="settings-line">
-      <p>
-        Removes all downloaded data (catalog, publications, videos etc.)
-        May fix issues with corrupted publications not showing properly.
-        This will send you back to the intro screen.
-      </p>
-      <button
-        :disabled="loading"
-        @click="onClearDownloads"
-      >
-        CLEAR DOWNLOADS
-      </button>
+    <div>
+      <div class="settings-line">
+        <p>
+          Removes all downloaded data (catalog, publications, videos etc.)
+          May fix issues with corrupted publications not showing properly.
+          This will send you back to the intro screen.
+        </p>
+        <button
+          :disabled="loading"
+          @click="onClearDownloads"
+        >
+          CLEAR DOWNLOADS
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -85,13 +89,19 @@ h2 {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 0 8px;
+  margin: 0 8px;
+  padding: 8px 0;
+}
+.settings-line:not(:last-child) {
+  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
 }
 .settings-line p {
   margin: 0;
 }
-.settings-line button {
-  width: 240px;
-  margin-left: 24px;
+.settings-line button,
+.settings-line select {
+  flex: 0 0 160px;
+  width: 160px;
+  margin: 0 0 0 24px;
 }
 </style>

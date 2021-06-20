3129de9d249aac075f7ae415f5e56e6b6feca298
BenShelton <bensheltonjones@gmail.com>
2021-06-20 12:55:20 +0100

    fix(media): show error dialog when media fails to load rather than infinite loader

--- packages/media/app/main/src/logger.ts
+++ packages/media/app/main/src/logger.ts
@@ -1,12 +1,17 @@
-import electron, { shell } from 'electron'
+import electron, { app, shell } from 'electron'
 import log from 'electron-log'
 
-export function submitIssue ({ versions, message }: { versions?: { app: string, os: string }, message: string}): void {
-  const { app = 'unknown', os = 'unknown' } = versions || {}
+function submitIssue ({ versions, message }: { versions?: { app: string, os: string }, message: string}): void {
+  const issueVersions = versions || { app: 'unknown', os: 'unknown' }
+  if (issueVersions.app === 'unknown') {
+    try {
+      issueVersions.app = `${app.name} ${app.getVersion()}`
+    } catch {}
+  }
   const body = '\n' +
     '**Packages & versions**\n' +
-    `Library Media ${app}\n` +
-    `OS: ${os}\n\n` +
+    `Library Media ${issueVersions.app}\n` +
+    `OS: ${issueVersions.os}\n\n` +
     '**Describe the bug**\n' +
     `${message}\n`
   const url = new URL('https://github.com/BenShelton/library-api/issues/new')
@@ -15,31 +20,33 @@ export function submitIssue ({ versions, message }: { versions?: { app: string, 
   shell.openExternal(url.toString())
 }
 
+export async function showErrorDialog (error: Error, versions?: { app: string, os: string }): Promise<void> {
+  const result = await electron.dialog.showMessageBox({
+    title: 'An error occurred',
+    message: error.message,
+    detail: error.stack,
+    type: 'error',
+    buttons: ['Ignore', 'Report', 'Exit']
+  })
+  if (result.response === 1) {
+    submitIssue({
+      versions,
+      message: 'Error:\n' +
+      '```\n' +
+      `${error.stack}\n` +
+      '```'
+    })
+    return
+  }
+
+  if (result.response === 2) {
+    electron.app.quit()
+  }
+}
+
 export function initLogger (): void {
   log.catchErrors({
     showDialog: false,
-    async onError (error, versions) {
-      const result = await electron.dialog.showMessageBox({
-        title: 'An error occurred',
-        message: error.message,
-        detail: error.stack,
-        type: 'error',
-        buttons: ['Ignore', 'Report', 'Exit']
-      })
-      if (result.response === 1) {
-        submitIssue({
-          versions,
-          message: 'Error:\n' +
-          '```\n' +
-          `${error.stack}\n` +
-          '```'
-        })
-        return
-      }
-
-      if (result.response === 2) {
-        electron.app.quit()
-      }
-    }
+    onError: showErrorDialog
   })
 }

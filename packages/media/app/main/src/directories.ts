dcb6115db0b6c8ae0fa13b4daf0b030612725229
BenShelton <bensheltonjones@gmail.com>
2021-07-26 11:49:37 +0100

    feat(media): forcefully delete old media catalogs on startup

--- packages/media/app/main/src/directories.ts
+++ packages/media/app/main/src/directories.ts
@@ -1,3 +1,5 @@
+import { readdir, unlink } from 'fs/promises'
+import { join } from 'path'
 import { createDir } from '@library-api/core'
 
 import { DOWNLOAD_DIR, VIDEO_DIR } from './constants'
@@ -5,4 +7,11 @@ import { DOWNLOAD_DIR, VIDEO_DIR } from './constants'
 export async function initDirectories (): Promise<void> {
   await createDir(DOWNLOAD_DIR)
   await createDir(VIDEO_DIR)
+  // TODO: Remove this when Media Catalogs are updateable
+  const files = await readdir(DOWNLOAD_DIR)
+  for (const file of files) {
+    if (file.endsWith('.ndjson')) {
+      await unlink(join(DOWNLOAD_DIR, file))
+    }
+  }
 }

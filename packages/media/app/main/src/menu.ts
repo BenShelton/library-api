66946b632b5498b660abec45c8457c699bd81894
BenShelton <bensheltonjones@gmail.com>
2021-07-26 12:07:44 +0100

    feat(media): add more menu items

--- packages/media/app/main/src/menu.ts
+++ packages/media/app/main/src/menu.ts
@@ -1,18 +1,53 @@
-import { Menu, shell } from 'electron'
+import { app, Menu, shell } from 'electron'
 import { MenuItemConstructorOptions } from 'electron/main'
 
-const appName = 'Library Media'
-const template: MenuItemConstructorOptions[] = [
-  {
-    label: appName,
+const isMac = process.platform === 'darwin'
+
+const template: MenuItemConstructorOptions[] = []
+
+if (isMac) {
+  template.push({
+    label: app.name,
     submenu: [
-      { role: 'about', label: `About ${appName}` },
-      { role: 'quit', label: 'Quit' }
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  })
+}
+template.push(
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' }
     ]
   },
   {
-    label: 'Help',
+    label: 'View',
     submenu: [
+      { role: 'togglefullscreen' }
+    ]
+  },
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Open Change Log',
+        click: async () => {
+          await shell.openExternal('https://github.com/BenShelton/library-api/blob/master/packages/media/CHANGELOG.md')
+        }
+      },
       {
         label: 'Open Documentation',
         click: async () => {
@@ -21,7 +56,7 @@ const template: MenuItemConstructorOptions[] = [
       }
     ]
   }
-]
+)
 
 export function initMenu (): void {
   const menu = Menu.buildFromTemplate(template)
